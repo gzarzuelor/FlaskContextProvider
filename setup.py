c771797fa937c54aea70ff45ec85afbd1359f1db
gzarrub <g.zarrub@gmail.com>
2015-02-04 19:10:32 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='FlaskContextProvider',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Guillermo',
+    author_email='gzarrub@gmail.com',
+    description=''
+)
