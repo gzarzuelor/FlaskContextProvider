c18cc3a39b8de955ea09fe9b9b6e113cd8c7e411
gzarrub <g.zarrub@gmail.com>
2015-02-13 10:19:01 +0100

    Corporate headers added

--- setup.py
+++ setup.py
@@ -1,3 +1,22 @@
+#!/usr/bin/python
+# Copyright 2015 Telefonica Investigacion y Desarrollo, S.A.U
+#
+# This file is part of FlaskContextProvider.
+#
+# FlaskContextProvider is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# FlaskContextProvider is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+# General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Orion Context Broker. If not, see http://www.gnu.org/licenses/.
+
+
 from setuptools import setup, find_packages
 
 setup(
@@ -6,7 +25,7 @@ setup(
     packages=find_packages(),
     url='https://github.com/gzarrub/FlaskContextProvider',
     license='',
-    author='Guillermo',
+    author='Guillermo Zarzuelo',
     author_email='gzarrub@gmail.com',
     description="""The FlaskContextProvider is a python software that creates a Flask server
     which allows to link a ContextBroker with another service. It is thought to provide real
--- sevici/sevici.py
+++ sevici/sevici.py
@@ -1,4 +1,22 @@
-__author__ = 'b.gzr'
+#!/usr/bin/python
+# Copyright 2015 Telefonica Investigacion y Desarrollo, S.A.U
+#
+# This file is part of FlaskContextProvider.
+#
+# FlaskContextProvider is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# FlaskContextProvider is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+# General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Orion Context Broker. If not, see http://www.gnu.org/licenses/.
+
+
 import xml.etree.ElementTree as ET
 import tools.DataManager as DM
 from xml.dom import minidom
